e805c4539fdcba9b363b289eb996f96d0be7eb19
luisdaniel-21 <pbohorquezluisdani@gmail.com>
2022-10-17 14:34:18 -0500

    mysencond commit

--- app/src/main/java/com/example/quotescelebrities/presentation/view/QuoteRandomActivity.kt
+++ app/src/main/java/com/example/quotescelebrities/presentation/view/QuoteRandomActivity.kt
@@ -1,41 +1,39 @@
 package com.example.quotescelebrities.presentation.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.example.quotescelebrities.R
-import com.example.quotescelebrities.databinding.ActivityMainBinding
-import com.example.quotescelebrities.presentation.view_model.QuoteViewModel
+import com.example.quotescelebrities.databinding.ActivityQuoteRandomBinding
+//import com.example.quotescelebrities.databinding.ActivityQuoteRandomBinding
+import com.example.quotescelebrities.presentation.view_model.QuoteRandomViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class QuoteRandomActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private val quoteViewModel: QuoteViewModel by viewModels()
+    private lateinit var binding: ActivityQuoteRandomBinding
+    private val quotRandomViewModel: QuoteRandomViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityQuoteRandomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //----------------------------
-        quoteViewModel.randomQuote()
+        quotRandomViewModel.randomQuote()
         observer()
         binding.viewContainer.setOnClickListener {
-            quoteViewModel.randomQuote()
+            quotRandomViewModel.randomQuote()
         }
 
     }
     private fun observer(){
         lifecycleScope.launch {
-            quoteViewModel.quoteModel.collect {
+            quotRandomViewModel.quoteModel.collect {
                 binding.tvQuote.text = it.quote
                 binding.tvAuthor.text= it.author
             }
@@ -44,4 +42,3 @@ class MainActivity : AppCompatActivity() {
 
 
 }
-
