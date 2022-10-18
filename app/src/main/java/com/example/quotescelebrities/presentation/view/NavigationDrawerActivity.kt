2d755d3410f3d6eb063de5f61e62f9b192a30020
luisdaniel-21 <pbohorquezluisdani@gmail.com>
2022-10-18 00:39:39 -0500

    Agregamos un navgation drawer

---
+++ app/src/main/java/com/example/quotescelebrities/presentation/view/NavigationDrawerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.quotescelebrities.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavigationDrawerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_drawer)
+    }
+}
